cfd9a6b4160cf95c99f0923aa7dd92b209a41b16
Sachin Selvakumar <sachin.selvakumar24@gmail.com>
2026-06-28 14:30:53 -0500

    Bulk sync: 10 submissions

---
+++ Data Structures & Algorithms/two-integer-sum/submission-12.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer,Integer> complement = new HashMap<>();
+        for(int i = 0; i < nums.length; i++){
+            Integer complementIndex = complement.get(nums[i]);
+            if(complementIndex != null){
+                return new int[]{complementIndex , i};
+            }
+            complement.put(target - nums[i] , i);
+        }
+
+        return new int[]{};
+        
+    }
+}
